cda6185088e97f6860ff18b8ea481a1cc1d222b4
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:04:18 +0530

    Zoho Set-1 Q2

--- SumOfWeights.java
+++ SumOfWeights.java
@@ -1,18 +1,30 @@
+/* 
+
+2. Given a set of numbers like <10, 36, 54,89,12> we want to find sum of weights based on the following conditions
+    1. 5 if a perfect square
+    2. 4 if multiple of 4 and divisible by 6
+    3. 3 if even number
+And sort the numbers based on the weight and print it as follows 
+
+<10,its_weight>,<36,its weight><89,its weight>
+Should display the numbers based on increasing order.
+
+*/
 public class SumOfWeights {
     public static void main(String args[])
     {
         int arr[] = {10,36,54,89,12};
         int temp;
 
-//        for(int i=0;i<arr.length;i++){
-//            for(int j=0;j<arr.length;j++){
-//                if(arr[i]<arr[j]){
-//                    temp=arr[i];
-//                    arr[i]=arr[j];
-//                    arr[j]=temp;
-//                }
-//            }
-//        }
+       for(int i=0;i<arr.length;i++){
+           for(int j=0;j<arr.length;j++){
+               if(arr[i]<arr[j]){
+                   temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+               }
+           }
+       }
 
         int wrr[] =new int[5];
 
