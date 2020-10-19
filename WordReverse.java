79406458e89ab18c8ee60a8170fb18a6ad7912d0
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:44:55 +0530

    Zoho Set-3 Q6

--- WordReverse.java
+++ WordReverse.java
@@ -1,3 +1,12 @@
+/*
+6. Using Recursion reverse the string such as
+
+Eg 1: Input: one two three
+      Output: three two one
+Eg 2: Input: I love india
+      Output: india love I 
+*/
+
 import java.util.Scanner;
 
 public class WordReverse {
