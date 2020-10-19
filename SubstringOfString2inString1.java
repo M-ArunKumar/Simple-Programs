7deafcd0caee83aef5f4fe496b323106d173e731
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:42:05 +0530

    Zoho Set-3 Q4

--- SubstringOfString2inString1.java
+++ SubstringOfString2inString1.java
@@ -1,3 +1,18 @@
+/*
+
+4. Find if a String2 is substring of String1. If it is, return the index of the first occurrence. else return -1.
+
+Eg 1:Input:
+        String 1: test123string
+         String 2: 123
+        Output: 4
+Eg 2: Input:
+        String 1: testing12
+        String 2: 1234 
+        Output: -1
+        
+*/
+
 import java.util.Scanner;
 
 public class SubstringOfString2inString1 {
