1e2f8c57b4fc9386d277eacf1b33b6f6dc4e27d0
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:47:47 +0530

    Zoho Set-3 Q2

--- ArrayMaxMinOrder.java
+++ ArrayMaxMinOrder.java
@@ -1,3 +1,12 @@
+/*
+2. Write a program to sort the elements in odd positions in descending order and elements in ascending order
+
+Eg 1: Input: 13,2 4,15,12,10,5
+        Output: 13,2,12,10,5,15,4
+Eg 2: Input: 1,2,3,4,5,6,7,8,9
+        Output: 9,2,7,4,5,6,3,8,1 
+*/
+
 public class ArrayMaxMinOrder {
     static int[] rearrange(int arr[], int n)
     {
