8dd681c5550f22afb6e20dbc48d87aa93cce2934
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:06:07 +0530

    Zoho Set-1 Q3

--- substringSearch.java
+++ substringSearch.java
@@ -1,3 +1,18 @@
+/*
+3. Save the string “WELCOMETOZOHOCORPORATION” in a two dimensional array and search for substring like “too” in the two dimensional string both 
+from left to right and from top to bottom.
+
+w	e	L	C	O
+M	E	T	O	Z
+O	H	O	C	O
+R	P	O	R	A
+T	I	O	n	  
+And print the start and ending index as
+
+Start index : <1,2>
+End index: <3, 2>
+*/
+
 import java.util.Scanner;
 
 public class substringSearch {
