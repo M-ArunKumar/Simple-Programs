350a129af3fa3512ec4fa9aaed80fd2fdd72fa7d
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:07:25 +0530

    Zoho Set-1 Q5

--- FindingGrandChildren.java
+++ FindingGrandChildren.java
@@ -1,3 +1,16 @@
+/* 
+
+5. Given a two dimensional array of string like
+
+  <”luke”, “shaw”>
+  <”wayne”, “rooney”>
+  <”rooney”, “ronaldo”>
+  <”shaw”, “rooney”> 
+Where the first string is “child”, second string is “Father”. And given “ronaldo” we have to find his no of grandchildren Here “ronaldo” has 2 grandchildren. 
+So our output should be 2.
+
+*/
+
 import java.util.Scanner;
 
 public class FindingGrandChildren {
