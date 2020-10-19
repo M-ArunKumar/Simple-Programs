933a3b54fc97083c8d5a28864560bc7301c84e8c
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-19 15:38:50 +0530

    Zoho Set-3 Q1

--- AlphabetOccurance.java
+++ AlphabetOccurance.java
@@ -1,3 +1,15 @@
+/*
+
+1. Write a program to give the following output for the given input
+
+Eg 1: Input: a1b10
+       Output: abbbbbbbbbb
+Eg: 2: Input: b3c6d15
+          Output: bbbccccccddddddddddddddd
+The number varies from 1 to 99.
+
+*/
+
 import java.util.Scanner;
 
 public class AlphabetOccurance {
