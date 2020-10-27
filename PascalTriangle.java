fd260cfa9410c7ebd169470aa2962e9bcd91816a
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-27 18:26:31 +0530

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,3 +1,29 @@
+/*
+4. Print the following pattern
+
+  Ex.  INPUT  :  5
+
+    OUTPUT  :
+
+            1
+           1 1
+          1 2 1
+         1 3 3 1
+        1 4 6 4 1
+
+  Ex.  INPUT  :  7
+
+    OUTPUT  :
+
+            1
+           1 1
+          1 2 1
+         1 3 3 1
+        1 4 6 4 1
+       1 5 10 10 5 1
+      1 6 15 20 15 6 1
+ */
+
 public class PascalTriangle {
     public static void main (String[] args) {
         int n = 7;
