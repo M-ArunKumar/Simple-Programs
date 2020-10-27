38cab583d8d647a95c6adef982a0a0120da0e975
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-27 19:50:25 +0530

    Update ConvertArrayNumber.java

--- ConvertArrayNumber.java
+++ ConvertArrayNumber.java
@@ -1,3 +1,15 @@
+/*
+
+2. Given an array of integers of size n. Convert the array in such a way that if next valid number is same as current number, double its value and replace the next number with 0. After the modification, rearrange the array such that all 0’s are shifted to the end.
+
+Input : arr[] = {2, 2, 0, 4, 0, 8}
+Output : 4 4 8 0 0 0
+
+Input : arr[] = {0, 2, 2, 2, 0, 6, 6, 0, 0, 8}
+Output : 4 2 12 8 0 0 0 0 0 0
+
+*/
+
 public class ConvertArrayNumber {
     static void pushZeros(int arr[],int n){
         int count = 0;
