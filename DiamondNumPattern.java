66e0fa5956bb45154a4cdb17cf860584efc21314
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-27 18:28:44 +0530

    Update DiamondNumPattern.java

--- DiamondNumPattern.java
+++ DiamondNumPattern.java
@@ -1,3 +1,18 @@
+/*
+
+3.Print the following pattern
+
+   1  
+  3 2
+ 6 5 4
+10 9 8 7
+10 9 8 7 
+ 6 5 4 
+  3 2 
+   1
+   
+*/
+
 public class DiamondNumPattern {
     public static void main(String[] args) {
         int n = 5;
