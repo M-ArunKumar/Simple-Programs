51a2c1ca5abb27f44563d8f1d851af2fdc461a96
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-28 16:23:57 +0530

    Zoho Set-2 Q3

--- NumberSystem.java
+++ NumberSystem.java
@@ -1,3 +1,10 @@
+/*
+
+3) Form a number system with only 3 and 4. Find the nth number of the number system.
+Eg.) The numbers are: 3, 4, 33, 34, 43, 44, 333, 334, 343, 344, 433, 434, 443, 444, 3333, 3334, 3343, 3344, 3433, 3434, 3443, 3444 .....
+
+*/
+
 import java.util.ArrayList;
 
 public class NumberSystem {
