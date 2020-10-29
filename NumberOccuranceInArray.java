ee89ccb6bd9722e4e7c1cee22ba3a4adf62f7d8c
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-29 20:42:52 +0530

    Zoho Set-10 Q3

--- NumberOccuranceInArray.java
+++ NumberOccuranceInArray.java
@@ -1,6 +1,6 @@
 /*
 
- Given a array with n elements print the number of occurrences of that number each number in that array. The order of number doesn’t matter. You can reorder the elements.
+Given a array with n elements print the number of occurrences of that number each number in that array. The order of number doesn’t matter. You can reorder the elements.
 Example : [2,1,3,2,2,5,8,9,8]
 Output:
 2-3
