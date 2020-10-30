bcdf3b3be9da5c6d360e35cfe76db98080500458
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-10-30 11:29:28 +0530

    Zoho Set-8 Q1

--- SpiralPrinting.java
+++ SpiralPrinting.java
@@ -1,3 +1,17 @@
+/*
+Spiral printing.
+
+Input = 4;
+Output:
+    4444444
+    4333334
+    4322234
+    4321234
+    4322234
+    4333334
+    4444444
+*/
+
 public class SpiralPrinting {
     static void pattern(int value)
     {
