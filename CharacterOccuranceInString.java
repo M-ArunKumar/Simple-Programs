3647ca0386964b014834be1588b7b41c45e05653
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-12-01 10:27:26 +0530

    Character occurance in a String

--- CharacterOccuranceInString.java
+++ CharacterOccuranceInString.java
@@ -3,13 +3,30 @@ import java.util.Map;
 
 public class CharacterOccuranceInString {
     public static void solution(String str) {
+        // Creating a HashMap containing char 
+        // as a key and occurrences as a value 
         Map<Character, Integer> map = new HashMap<>();
+        
         char[] arr = str.toCharArray();
         for (char c : arr) {
-            if (map.containsKey(c))
+            if (map.containsKey(c)){
+                 // If character is present 
+                // in map incrementing it's 
+                // count by 1 
                 map.put(c, map.get(c) + 1);
-            else map.put(c, 1);
+            }
+            else {
+                // If character is not present 
+                // in map putting this 
+                // character into map with 
+                // 1 as it's value. 
+                map.put(c, 1);
+            }
         }
+        // Traverse the HashMap, check 
+        // if the count of the character 
+        // is greater than 1 then print 
+        // the character and its frequency 
         for (Map.Entry<Character,Integer> entry : map.entrySet()){
             if (entry.getValue() > 1){
                 System.out.println(entry.getKey() + " : " + entry.getValue());
