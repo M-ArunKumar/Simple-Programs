e932b13ba2f9349f0b04ba379b4ab669d9e2c0b5
M-ArunKumar <72726588+M-ArunKumar@users.noreply.github.com>
2020-12-01 10:45:31 +0530

    Add files via upload

---
+++ PushingZerosToLast.java
@@ -0,0 +1,20 @@
+public class PushingZerosToLast {
+    public static void solution(int[] arr,int n){
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0)
+                arr[count++] = arr[i];
+        }
+        while (count < n)
+            arr[count++] = 0;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
+        int n = arr.length;
+        solution(arr,n);
+        System.out.println("After pushing zeros to last: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
